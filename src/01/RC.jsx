9051dd5a8e6f1dddcaddb059c2a527582cc0542f
Just[in]Do it! <justin.park@airbnb.com>
2022-04-22 15:27:22 -0700

    useTab 끄기

--- src/01/RC.jsx
+++ src/01/RC.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class RC extends React.Component {
-	render() {
-		var text = '따옴표';
-		return <div name="name">{text}</div>;
-	}
+  render() {
+    var text = '따옴표';
+    return <div name="name">{text}</div>;
+  }
 }
 export default RC;
