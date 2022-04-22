1c308dbceb240b2cc1dd556d6d3fec3206f098b8
Just[in]Do it! <justin.park@airbnb.com>
2022-04-22 15:25:58 -0700

    01-2 예제

---
+++ src/01/RC.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class RC extends React.Component {
+	render() {
+		var text = '따옴표';
+		return <div name="name">{text}</div>;
+	}
+}
+export default RC;
